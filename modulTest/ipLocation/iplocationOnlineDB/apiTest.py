551d8d1d136b23cc7fc27d222d1d7d56b98f2345
chengggguo <chengggguo@gmail.com>
2018-10-28 16:06:47 +0800

    swift os

--- modulTest/ipLocation/iplocationOnlineDB/apiTest.py
+++ modulTest/ipLocation/iplocationOnlineDB/apiTest.py
@@ -8,10 +8,10 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 
 url = "https://mall.ipplus360.com/ip/locate/api"
-data = {'key':'fRQk7uOaZZbZ1pZD8A5WmYQ5fXsTpitcbtgA7v6y8V6qXn82G6M4fIyoxzXwtljk','ip':'223.72.48.58','coordsys':'BD09','area':'multi'}
+data = {'key':'Mmz6TBcuRRkWJh4MHdUeQfC9vym8gBJVJZ8faAKRpQHVZEye3QrKwIYHVDUB14Uo','ip':'223.72.48.58','coordsys':'WGS84','area':'multi'}
 data = urllib.parse.urlencode(data)
 url = url + '?' + data
-print(url)
+#print(url)
 
 response = urllib.request.urlopen(url)
 
