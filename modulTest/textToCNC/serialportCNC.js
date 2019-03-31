579832a28da3124d640001ef6f3654f2e718bed5
chengggguo <chengggguo@gmail.com>
2019-03-31 14:46:31 +0800

    test the serialport communication

--- modulTest/textToCNC/serialportCNC.js
+++ modulTest/textToCNC/serialportCNC.js
@@ -3,6 +3,11 @@ const Readline = require('@serialport/parser-readline');
 const port = new SerialPort('/dev/ttyUSB0', { baudRate: 115200 });
 
 const parser = new Readline();
+
+var fs = require('fs');
+
+
+
 port.pipe(parser);
 
 port.on('open',function(){
